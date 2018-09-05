9bfa6750442f0f0a6a9e209b97b26c06b8670102
JEFFGOU <JEFFGOU@DESKTOP-2NUB4JN>
2018-09-05 23:43:13 +0800

    提交编译问题  以及对寻址的理解

--- CMemoryMe/main.cpp
+++ CMemoryMe/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <map>
 #include <ComprehandVirtual.h>
+#include <stdio.h>
 
 void testTemplateMap()
 {
@@ -20,11 +21,32 @@ void testTemplateMap()
     return;
 }
 
+void justAddress()
+{
+    //%p 这是地址寻址所以64位 8字节 32位 4字节。  而%x 是代表int型的对于64位软件就有问题了
+    //int a = 10;
+    //printf("a地址: %p = %d = 0x%x. 占用 %d个字节\n", &ba, &ba, &ba, sizeof(&a));
+    //std::cout << "a地址:" << static_cast<void *>(&a);
+}
+
+typedef void(*Func)(void);
+
 int main()
 {
-    JP::Base ba;
+    JP::Base *ba = new JP::Base;
 
-    ba.vFunc1();
+    //std::cout << "虚函数地址: " << (int *)(&ba) << std::endl;
+    //printf("虚函数地址: %p\n", &ba);
+
+    //Func pFun = (Func)*((int *)(*(int *)(&ba)));
+    //pFun = (Func)*((int *)*ba);
+
+    //pFun();
+    ba->vFunc1();
+
+    JP::Base *bap = new JP::Base;
+
+    bap->vFunc1();
 
     getchar();
     return 0;
